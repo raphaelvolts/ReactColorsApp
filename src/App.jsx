909991821ca44259b8109737d99246dddb471a9f
Anuranjan Gari <anuranjan.gari08@gmail.com>
2024-09-23 12:58:19 +0200

    added React Router and the Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { Routes, Route, useParams } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import generatePalette from "./chromaColorHelper";
 
 function App() {
-  console.log(generatePalette(seedColors[4]));
-  return <Palette palette={generatePalette(seedColors[4])} />;
+  function findPalette(id) {
+    return seedColors.find((palette) => {
+      return palette.id === id;
+    });
+  }
+  function DisplayPalette() {
+    let { id } = useParams();
+    return <Palette palette={generatePalette(findPalette(id))} />;
+  }
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Palette List goes here</h1>} />
+      <Route path="/palette/:id" element={<DisplayPalette />} />
+    </Routes>
+  );
 }
 
 export default App;
