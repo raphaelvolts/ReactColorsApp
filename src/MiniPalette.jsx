565414bf280f5d6c1552fc2d81df4ebee9ac0009
Anuranjan Gari <anuranjan.gari08@gmail.com>
2024-09-26 18:57:00 +0200

    almost completed styling MiniPalettes and added navigation to the complete palette page

--- src/MiniPalette.jsx
+++ src/MiniPalette.jsx
@@ -12,7 +12,11 @@ const myStyles = createUseStyles({
     }
   },
   color: {
-    backgroundColor: "gray"
+    backgroundColor: "gray",
+    height: "150px",
+    width: "100%",
+    borderRadius: "5px",
+    overflow: "hidden"
   },
   title: {
     display: "flex",
@@ -21,6 +25,7 @@ const myStyles = createUseStyles({
     margin: "0",
     color: "black",
     paddingTop: "0.5rem",
+    paddingBottom: "20px",
     fontSize: "1rem",
     position: "relative"
   },
@@ -28,15 +33,36 @@ const myStyles = createUseStyles({
     fontFamily: "NOTO COLOR EMOJI",
     fontSize: "1.5rem",
     marginLeft: "0.5rem"
+  },
+  miniColor: {
+    height: "25%",
+    width: "20%",
+    display: "inline-block",
+    margin: "0 auto",
+    marginBottom: "-3.5px",
+    position: "relative",
+    backgroundColor: (color) => color
   }
 });
 
-export default function MiniPalette({ palette }) {
+const MiniColorDiv = ({ color }) => {
+  const classes = myStyles(color);
+  return <div className={classes.miniColor}></div>;
+};
+
+export default function MiniPalette({ palette, navigation }) {
   const { paletteName, id, emoji, colors } = palette;
   const classes = myStyles();
+  function handleClick() {
+    navigation(id);
+  }
   return (
-    <div className={classes.root}>
-      <div className={classes.colors}></div>
+    <div className={classes.root} onClick={handleClick}>
+      <div className={classes.color}>
+        {colors.map((color) => (
+          <MiniColorDiv color={color.color} key={color.name} />
+        ))}
+      </div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
       </h5>
--- src/PaletteList.jsx
+++ src/PaletteList.jsx
@@ -1,16 +1,63 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
+import { createUseStyles } from "react-jss";
 import "./PaletteList.css";
 
+const listPageStyles = createUseStyles({
+  root: {
+    backgroundColor: "navy",
+    display: "flex",
+    height: "100%",
+    alignItems: "flex-start",
+    justifyContent: "center"
+  },
+  container: {
+    display: "flex",
+    width: "50%",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap"
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    color: "white"
+  },
+  palettes: {
+    width: "100%",
+    boxSizing: "border-box",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gap: "5%",
+    marginTop: "30px"
+  }
+});
+
 export default function PaletteList({ palettes }) {
+  const classes = listPageStyles();
+  const navigate = useNavigate();
+  function goToPalette(id) {
+    navigate(`/palette/${id}`);
+  }
   let links = palettes.map((palette) => {
-    return <MiniPalette key={palette.id} palette={palette} />;
+    return (
+      <MiniPalette
+        key={palette.id}
+        palette={palette}
+        navigation={goToPalette}
+      />
+    );
   });
 
   return (
-    <div className="PaletteList">
-      <h1>React Color Palettes List</h1>
-      {links}
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>React Color Palettes</h1>
+        </nav>
+        <div className={classes.palettes}>{links}</div>
+      </div>
     </div>
   );
 }
